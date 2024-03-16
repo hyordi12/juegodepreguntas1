44443ff305dbcd52fc5d17271f16f4a59f9a5234
unknown <hyordi12>
2024-03-16 10:31:58 -0600

    se empieza a agregar la logica del juego

--- app/src/main/java/com/example/juegodepreguntas1/MainActivity.java
+++ app/src/main/java/com/example/juegodepreguntas1/MainActivity.java
@@ -1,10 +1,23 @@
 package com.example.juegodepreguntas1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView questionTextView;
+    private Button optionOneButton;
+    private Button optionTwoButton;
+    private TextView resultTextView;
+    private int currentQuestionIndex = 0;
+    private Question[] questions = new Question[8];
+    private Random random = new Random();
+    private int correctAnswers = 0;
+    private int incorrectAnswers = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
